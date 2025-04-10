82d801ba6499e1f0826dbdde8cf2d99e0128a2eb
Def-fect <maxrah@metropolia.fi>
2025-04-10 12:45:04 +0300

    Tehtavat

--- moduuli1/teht5/teht5.js
+++ moduuli1/teht5/teht5.js
@@ -1,1 +1,25 @@
-const vuosi parseInt(prompt('anna vuosiluku, onko se'))
+const vuosi = parseInt(prompt('anna vuosiluku, onko se'));
+
+if (vuosi /= 4 ) {
+  console.log('The number is positive.');
+} else if (number < 0) {
+  console.log('The number is negative.');
+} else {
+  console.log('That number is zero.');
+}
+
+console.log(vastaus);
+
+
+
+num = int(input("Anna luku"))
+
+if num > 1:
+    for i in range(2, (num // 2) + 1):
+        if (num % i) == 0:
+            print(num, "ei ole alkuluku")
+            break
+    else:
+        print(num, "luku on alkuuku")
+else:
+    print(num, "ei ole alkuluku")
